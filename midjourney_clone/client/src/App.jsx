2f235aa22a9a796d700a3e98390803f80fa68917
Vansh Billimoria <vanshb482@gmail.com>
2024-01-05 17:43:37 +0530

    update app.jsx

--- midjourney_clone/client/src/App.jsx
+++ midjourney_clone/client/src/App.jsx
@@ -8,14 +8,10 @@ const App = () => {
     <Router>
       <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
         <Link to='/'>
-          <img src={logo} alt="Logo" className='w-28 object-contain' />
+            
         </Link>
-        <Link to='/create_post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link>
+        <Link to='/create_post' className='font-inter font-medium bg-[#454ad4] text-white px-4 py-2 rounded-md'>Create</Link>
       </header>
-      <Routes>
-        <Route path='/' element={<home />} />
-        <Route path='/create_post' element={<create_post />} />
-      </Routes>
     </Router>
   );
 }
