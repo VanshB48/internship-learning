0c15898ec262ad4c7ac345713df770172d0ab27a
Vansh Billimoria <vanshb482@gmail.com>
2024-01-05 17:27:02 +0530

    Update App.jsx

--- midjourney_clone/client/src/App.jsx
+++ midjourney_clone/client/src/App.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
-import {logo} from './assets';
-import {home,create_post} from './pages';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import { logo } from './assets';
+import { home, create_post } from './pages';
 
-const App = () =>{
-  return(
-    <BrowserRouter>
-      <header className='w-full flex justify between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
-        <link to='/'>
-          <img src={logo} alt="Logo" className='w-28 object-contain'/>
-        </link>
-        <link to='./create_post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</link>
+const App = () => {
+  return (
+    <Router>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <Link to='/'>
+          <img src={logo} alt="Logo" className='w-28 object-contain' />
+        </Link>
+        <Link to='/create_post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link>
       </header>
-    </BrowserRouter>
-  )
+      <Routes>
+        <Route path='/' element={<home />} />
+        <Route path='/create_post' element={<create_post />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
