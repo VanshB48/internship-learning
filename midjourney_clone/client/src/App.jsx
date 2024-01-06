bda6f4da8892b82be91ad23849a42233e53d1df5
Vansh Billimoria <vanshb482@gmail.com>
2024-01-06 12:53:05 +0530

    Update App.jsx

--- midjourney_clone/client/src/App.jsx
+++ midjourney_clone/client/src/App.jsx
@@ -3,15 +3,21 @@ import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import { logo } from './assets';
 import { home, create_post } from './pages';
 
+
 const App = () => {
   return (
     <Router>
       <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
-        <Link to='/'>
-            
+        <Link to='/'>            
         </Link>
         <Link to='/create_post' className='font-inter font-medium bg-[#454ad4] text-white px-4 py-2 rounded-md'>Create</Link>
       </header>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh - 73px)] '>
+        <Routes>
+          <Route path='/' element={<home />} />
+          <Route path='/create_post' element={<create_post />} />
+        </Routes>
+      </main>
     </Router>
   );
 }
