0f92fac21f06df935618304fd550ca7b29567856
Vansh Billimoria <vanshb482@gmail.com>
2024-01-08 13:31:28 +0530

    logo2 deleted home updated

--- midjourney_clone/client/src/pages/create_post.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const create_post = () => {
-  return (
-    <div>create_post</div>
-  )
-}
-
-export default create_post
--- midjourney_clone/client/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import home from './home.jsx'
-import create_post from './create_post.jsx'
-
-export{
-    home,
-    create_post
-}
